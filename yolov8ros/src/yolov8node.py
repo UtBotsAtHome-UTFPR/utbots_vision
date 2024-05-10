28ddcf907646c1c3e533fa0b120e866d139b205e
David Segalle <davidsegalle@alunos.utfpr.edu.br>
2024-05-10 15:15:11 -0300

    Added enable and service to yolo node

--- yolov8ros/src/yolov8node.py
+++ yolov8ros/src/yolov8node.py
@@ -8,6 +8,8 @@ import supervision as sv
 import rospy
 from sensor_msgs.msg import Image
 from vision_msgs.msg import BoundingBoxes, BoundingBox
+from std_msgs.msg import Bool
+from std_srvs.srv import Empty
 from cv_bridge import CvBridge
 
 class ObjectDetectionLive:
@@ -23,16 +25,19 @@ class ObjectDetectionLive:
         self.box_annotator = sv.BoxAnnotator(sv.ColorPalette.default(), thickness=2, text_thickness=1, text_scale=0.75)
         self.bridge = CvBridge()
         self.cv_img = None
+        self.msg_enable = False
         
         # Messages
         self.msg_img = Image()
         self.msg_bounding_boxes = BoundingBoxes()
         
-        # Subscribers and Publishers
-        self.sub_frame = rospy.Subscriber("/camera/rgb/image_color", Image, callback=self.callback_img)
+        # Subscribers and Publishers and Services
+        self.sub_frame = rospy.Subscriber("/usb_cam/image_raw", Image, callback=self.callback_img)
+        self.sub_enable = rospy.Subscriber("/utbots/vision/detection/enable", Bool, callback=self.callback_enable)
         self.pub_detection_img = rospy.Publisher("/utbots/vision/detection/image", Image, queue_size=1)
         self.pub_bounding_boxes = rospy.Publisher("/utbots/vision/detection/bounding_boxes", BoundingBoxes, queue_size=1)
-        
+        self.detect_img_service = rospy.Service('/utbots/vision/detection_srv', Empty, self.detection_srv)
+
         # Node initialization
         rospy.init_node('yolov8_live', anonymous=True)
         self.rate = rospy.Rate(30) # 30hz
@@ -42,6 +47,17 @@ class ObjectDetectionLive:
     def callback_img(self, msg):
         self.msg_img = msg
         self.cv_img = self.bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")
+
+    def callback_enable(self, msg):
+        self.msg_enable = msg.data
+
+    def detection_srv(self, msg):
+        if self.cv_img is not None:
+            results = self.predict(self.cv_img)
+            frame = self.plot_bboxes(results, self.cv_img)
+            self.pub_detection_img.publish(self.bridge.cv2_to_imgmsg(frame, encoding="passthrough"))
+            self.pub_bounding_boxes.publish(self.msg_bounding_boxes)
+        return []
 
     def load_model(self):
         model = YOLO(self.weights)  # load a pretrained YOLOv8n model
@@ -61,8 +77,8 @@ class ObjectDetectionLive:
                     confidence=results[0].boxes.conf.cpu().numpy(),
                     class_id=results[0].boxes.cls.cpu().numpy().astype(int),    # numeric indexes for the infered classes
                     )
-        
-        for coordinates, _, confidence, class_id, _ in detections:
+
+        for coordinates, _, confidence, class_id, _, _ in detections:
             # Assemble BoudingBox object
             bbox = BoundingBox()
             bbox.Class = self.CLASS_NAMES_DICT[class_id]                        # given the class index, returns the name of the class
@@ -72,7 +88,7 @@ class ObjectDetectionLive:
             self.msg_bounding_boxes.bounding_boxes.append(bbox)
 
         self.labels = [f"{self.CLASS_NAMES_DICT[class_id]} {confidence:0.2f}"
-        for _, _, confidence, class_id, _
+        for _, _, confidence, class_id, _, _
         in detections]
         
         # Annotate frame
@@ -83,7 +99,7 @@ class ObjectDetectionLive:
     def main(self):
         rospy.loginfo(f"Using Device: {self.device}")
         while not rospy.is_shutdown():
-            if self.cv_img is not None:
+            if self.cv_img is not None and self.msg_enable:
                 start_time = time()
                 results = self.predict(self.cv_img)
                 frame = self.plot_bboxes(results, self.cv_img)
