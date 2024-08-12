9d17693fb46e6d4ed6d7a308b56557cbbaea00a7
David Segalle <davidsegalle@alunos.utfpr.edu.br>
2024-08-12 18:53:25 -0300

    node now runs with service enabled synchronous detection and assynchronous action

---
+++ yolov8ros/README.md
@@ -0,0 +1,87 @@
+# yolov8ros
+- ROS package that implements [YOLOv8](https://github.com/ultralytics/ultralytics) 
+- Synchronous detection is Enabled/Disabled with a ROS service (disabled by standard)
+- Assynchronous detection is possible with actions. Optional target category and success result for finding specific category bounding boxes.
+
+## Installation
+
+### Building (if downloaded outside utbots_vision)
+
+```bash
+cd ~/catkin_ws/src
+git clone https://github.com/UtBotsAtHome-UTFPR/yolov8ros.git
+cd ..
+catkin_make
+```
+
+### Dependencies
+
+This package must be used alongside the [utbots_dependencies](https://github.com/UtBotsAtHome-UTFPR/utbots_dependencies) as it uses some of the message and action definitions. You can do this with:
+
+```bash
+cd ~/catkin_ws/src/
+git clone https://github.com/UtBotsAtHome-UTFPR/utbots_dependencies.git
+cd ..
+catkin_make
+```
+
+The code runs on Python 3.8. Install YOLO and other Python requirements:
+
+```bash
+roscd yolov8ros
+pip install -r requirements.txt
+```
+
+**OBS:** Recommended Python virtual environment
+
+
+## Running
+
+First, initialize ROS (if not already):
+```bash
+roscore
+```
+Then, run the node:
+```bash
+rosrun yolov8ros yolov8node.py
+```
+### Getting detections
+The enable control can be made inside a script with a ServiceClient. To enable/disable in the terminal:
+```bash
+rosservice call /utbots/vision/enable_detection <"False" or "True">                          
+```
+To trigger a detection response with an action in the terminal. Optionally, it is possible to send a category filter filling `<optional category name>` optional goal:
+```bash
+rostopic pub /YOLO_detection/goal utbots_actions/YOLODetectionActionGoal "header:
+  seq: 0
+  stamp:
+    secs: 0
+    nsecs: 0
+  frame_id: ''
+goal_id:
+  stamp:
+    secs: 0
+    nsecs: 0
+  id: ''
+goal:
+  TargetCategory:
+    data: '<optional category name>'
+  Image:
+    header:
+      seq: 0
+      stamp: {secs: 0, nsecs: 0}
+      frame_id: ''
+    height: 0
+    width: 0
+    encoding: ''
+    is_bigendian: 0
+    step: 0
+    data: []"
+```
+**OBS**: Images are not easy to sent in the terminal, so the optional Image goal should be used only when using ActionClients inside scripts.
+
+To see the action result in the terminal:
+```bash
+rostopic echo /YOLO_detection/result
+```
+**OBS**: The Image result isa huge matrix normally, so you may not be able to see the whole result msg
--- yolov8ros/src/yolov8node.py
+++ yolov8ros/src/yolov8node.py
@@ -9,8 +9,10 @@ import rospy
 from sensor_msgs.msg import Image
 from vision_msgs.msg import BoundingBoxes, BoundingBox
 from std_msgs.msg import Bool
-from std_srvs.srv import Empty
+from std_srvs.srv import SetBool, SetBoolResponse
 from cv_bridge import CvBridge
+import actionlib
+from utbots_actions.msg import YOLODetectionAction, YOLODetectionResult
 
 class ObjectDetectionLive:
 
@@ -33,14 +35,16 @@ class ObjectDetectionLive:
         
         # Subscribers and Publishers and Services
         self.sub_frame = rospy.Subscriber("/usb_cam/image_raw", Image, callback=self.callback_img)
-        self.sub_enable = rospy.Subscriber("/utbots/vision/detection/enable", Bool, callback=self.callback_enable)
         self.pub_detection_img = rospy.Publisher("/utbots/vision/detection/image", Image, queue_size=1)
         self.pub_bounding_boxes = rospy.Publisher("/utbots/vision/detection/bounding_boxes", BoundingBoxes, queue_size=1)
-        self.detect_img_service = rospy.Service('/utbots/vision/detection_srv', Empty, self.detection_srv)
+        self.detect_img_service = rospy.Service('/utbots/vision/enable_detection', SetBool, self.enable_detection)
 
         # Node initialization
         rospy.init_node('yolov8_live', anonymous=True)
-        self.rate = rospy.Rate(30) # 30hz
+        self.rate = rospy.Rate(1) # 1hz
+        # Action server initialization
+        self._as = actionlib.SimpleActionServer('YOLO_detection', YOLODetectionAction, self.detection_action, False)
+        self._as.start()
         
         self.main()
     
@@ -48,15 +52,43 @@ class ObjectDetectionLive:
         self.msg_img = msg
         self.cv_img = self.bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")
 
-    def callback_enable(self, msg):
+    def enable_detection(self, msg):
         self.msg_enable = msg.data
+        self.response = SetBoolResponse()
+        self.response.success = True
+        return self.response
 
-    def detection_srv(self, msg):
-        if self.cv_img is not None:
-            results = self.predict(self.cv_img)
-            frame = self.plot_bboxes(results, self.cv_img)
-            self.pub_detection_img.publish(self.bridge.cv2_to_imgmsg(frame, encoding="passthrough"))
+    def detection_action (self, goal):
+        # Optional specific image sent as goal
+        if goal.Image.width != 0 and goal.Image.height != 0:
+            print( goal.Image.width )
+            image = self.bridge.imgmsg_to_cv2(goal.Image, desired_encoding="bgr8")
+        else:
+        # Topic subscribed image
+            image = self.cv_img
+
+        # Target Category will filter bboxes. "" if no target
+        target_category = goal.TargetCategory.data
+        
+        if image is not None:
+            results = self.predict(image) # Model output
+            frame, bboxes = self.plot_bboxes(results, image, target_category) # Labeled frame and BoundingBoxes msg
+            frame = self.bridge.cv2_to_imgmsg(frame, encoding="passthrough") # Convert to img_msg
+
+            # Manage action results
+            action_res = YOLODetectionResult()
+            action_res.DetectedObjs = bboxes
+            action_res.LabeledImage = frame
+            action_res.Success = Bool()
+            if target_category != "":
+                if bboxes.bounding_boxes is not []:
+                    action_res.Success.data = True
+                else:
+                    action_res.Success.data = False
+
+            self.pub_detection_img.publish(frame)
             self.pub_bounding_boxes.publish(self.msg_bounding_boxes)
+            self._as.set_succeeded(action_res)
         return []
 
     def load_model(self):
@@ -68,7 +100,7 @@ class ObjectDetectionLive:
         results = self.model(frame) # sends the image to the model and returns results
         return results
     
-    def plot_bboxes(self, results, frame):
+    def plot_bboxes(self, results, frame, target_category):
         self.msg_bounding_boxes = BoundingBoxes()
         
         # Setup detections for visualization
@@ -79,13 +111,15 @@ class ObjectDetectionLive:
                     )
 
         for coordinates, _, confidence, class_id, _ in detections:
-            # Assemble BoudingBox object
-            bbox = BoundingBox()
-            bbox.Class = self.CLASS_NAMES_DICT[class_id]                        # given the class index, returns the name of the class
-            bbox.probability = confidence
-            bbox.xmin, bbox.ymin, bbox.xmax, bbox.ymax = int(coordinates[0]), int(coordinates[1]), int(coordinates[2]), int(coordinates[3])
-            # Append BoundingBox to a BoundingBoxes message
-            self.msg_bounding_boxes.bounding_boxes.append(bbox)
+                # Filters category if not empty
+                if (target_category != "" and self.CLASS_NAMES_DICT[class_id] == target_category) or target_category == "": 
+                    # Assemble BoudingBox object
+                    bbox = BoundingBox()
+                    bbox.Class = self.CLASS_NAMES_DICT[class_id]                        # given the class index, returns the name of the class
+                    bbox.probability = confidence
+                    bbox.xmin, bbox.ymin, bbox.xmax, bbox.ymax = int(coordinates[0]), int(coordinates[1]), int(coordinates[2]), int(coordinates[3])
+                    # Append BoundingBox to a BoundingBoxes message
+                    self.msg_bounding_boxes.bounding_boxes.append(bbox)
 
         self.labels = [f"{self.CLASS_NAMES_DICT[class_id]} {confidence:0.2f}"
         for _, _, confidence, class_id, _
@@ -93,8 +127,10 @@ class ObjectDetectionLive:
         
         # Annotate frame
         frame = self.box_annotator.annotate(scene=frame, detections=detections, labels=self.labels)
+
+        bboxes = self.msg_bounding_boxes
     
-        return frame
+        return frame, bboxes
     
     def main(self):
         rospy.loginfo(f"Using Device: {self.device}")
@@ -102,7 +138,7 @@ class ObjectDetectionLive:
             if self.cv_img is not None and self.msg_enable:
                 start_time = time()
                 results = self.predict(self.cv_img)
-                frame = self.plot_bboxes(results, self.cv_img)
+                frame, _ = self.plot_bboxes(results, self.cv_img, "")
                 
                 # Calculate frames per second
                 end_time = time()
