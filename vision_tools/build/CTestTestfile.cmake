ac1620bc481f692292f5021c9c498b9040d9ee0c
laser <utbots.home@gmail.com>
2025-04-03 17:06:02 -0300

    cleanup of ros1 packages and add mediapipe ros2

--- vision_tools/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/laser/catkin_ws/src/img_converter
-# Build directory: /home/laser/catkin_ws/src/img_converter/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
