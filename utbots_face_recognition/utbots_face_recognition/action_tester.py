34e5d1f25315d4501deeaf39f6a71f8e95d23706
David Segalle <davidsegalle@alunos.utfpr.edu.br>
2025-09-05 15:33:31 -0300

    getting segmentation distance from kinect apparently

--- utbots_face_recognition/utbots_face_recognition/action_tester.py
+++ utbots_face_recognition/utbots_face_recognition/action_tester.py
@@ -38,7 +38,7 @@ def main(args=None):
     action_client = RecognizeActionClient()
 
     future = action_client.send_new_face_goal()
-    rclpy.spin_until_future_complete(action_client, future)
+    #future = action_client.send_recognition_goal()
 
 if __name__ == '__main__':
     main()
